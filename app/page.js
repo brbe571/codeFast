76ceb1c662217a69141deb8f83321fb672dd87eb
brbe571 <brbe571@gmail.com>
2026-01-23 18:46:39 +1100

    added auth

--- app/page.js
+++ app/page.js
@@ -4,11 +4,12 @@ import ButtonLogin from "@/components/ButtonLogin";
 import ListItem from "@/components/ListItem";
 import FAQListItem from "@/components/FAQListItem";
 import productDemo from "./productDemo.jpeg";
+import { auth } from "@/auth";
 
 
-export default function Home() {
-  const isLoggedIn = true;
-  const name = "Ben";
+export default async function Home() {
+  
+  const session = await auth();
 
   
 
@@ -17,13 +18,13 @@ export default function Home() {
       {/* HEADER */}
       <section className="bg-base-200" id="header">
           <div className="bg-base-200 flex justify-between items-center py-2 px-8 max-w-5xl mx-auto">
-            <div>CodeFastSaas</div>
+            <div>ListingSend</div>
             <div className="space-x-4 max-md:hidden">
               <a className="link link-hover" href="#pricing">Pricing</a>
               <a className="link link-hover" href="#FAQ">FAQ</a>
             </div>
             <div>
-              <ButtonLogin isLoggedIn={isLoggedIn} name={name} />
+              <ButtonLogin session={session} />
             </div>
           </div>
       </section>
@@ -32,34 +33,34 @@ export default function Home() {
 
         <Image src={productDemo} alt="Product Demo" className="w-96 rounded-xl"/>
         <div>
-          <h1 className='text-4xl lg:text-5xl font-extrabold mb-6'>Collect Customer feedback to build better products</h1>
-          <div className='opacity-90 mb-10'>Create a feedback board in minutes, prioritize features, abd build products your customers will love</div>
+          <h1 className='text-4xl lg:text-5xl font-extrabold mb-6'>Send, Track and Store Property Information Securely </h1>
+          <div className='opacity-90 mb-10'>Reduce Admin time and track prospective buyers</div>
 
-          <ButtonLogin isLoggedIn={isLoggedIn} name={name} />
+          <ButtonLogin session={session} />
         </div>
       </section>
       {/* PRICING */ }
       <section className="bg-base-200" id="pricing">
         <div className="py-32 px-8 max-w-3xl mx-auto">
           <p className="text-sm uppercase font-medium text-center text-primary mb-4">Pricing</p>
-          <h2 className="text-3xl lg:text-4xl font-extrabold mb-12 text-center">A pricing that adapts to your needs</h2>
+          <h2 className="text-3xl lg:text-4xl font-extrabold mb-12 text-center">A one time payment per listing</h2>
         
 
           <div className="p-8 bg-base-100 w-96 rounded-3xl mx-auto space-y-6">
             <div className="flex gap-2 items-baseline">
               <div className="font-black text-4xl">$19</div>
-              <div className="uppercase text-sm font-medium opacity-60">/month</div>
+              <div className="uppercase text-sm font-medium opacity-60">/listing</div>
             </div>
 
             <ul className="space-y-2">
-              <ListItem>Collect Feedback</ListItem>
-              <ListItem>Unlimited Boards</ListItem>
-              <ListItem>Admin Dashboard</ListItem>
+              <ListItem>Send Property Info packs</ListItem>
+              <ListItem>Track views and accessed documents</ListItem>
+              <ListItem>Store</ListItem>
               <ListItem>24/7 Support</ListItem>
             </ul>
               
             
-            <ButtonLogin isLoggedIn={isLoggedIn} name={name} extraStyle="w-full" />
+            <ButtonLogin session={session} extraStyle="w-full" />
             
           </div>
         </div>
